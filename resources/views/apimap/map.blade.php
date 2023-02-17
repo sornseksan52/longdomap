7fc0b80160477b60c1c97e047ffac45811695274
เสก <developjunioradv@gmail.com>
2023-02-17 14:19:55 +0700

    init longdo map add input

--- resources/views/apimap/map.blade.php
+++ resources/views/apimap/map.blade.php
@@ -17,19 +17,37 @@
             height: 100%;
         }
         #map {
-            height: 100%;
+            height: 70%;
+            width: 100%;
         }
     </style>
-    <body onload="init();">
-        <div id="map"></div>
+    <body>
+        <div class="container-fluid">
+
+            <div class="input-group mt-3 mb-3">
+                <input type="text" class="form-control" placeholder="Search">
+                <div class="input-group-append">
+                  <button class="btn btn-success" type="button">Search</button>
+                </div>
+            </div>
+
+            <div class="text-ceenter">
+                <div id="map"></div>
+            </div>
+        </div>
 
         <script type="text/javascript" src="https://api.longdo.com/map/?key=a1f0b3aff58245ed8be4929f110ba0b5"></script>
+
         <script>
+
           function init() {
             var map = new longdo.Map({
               placeholder: document.getElementById('map')
             });
           }
+
+          init();
+
         </script>
     </body>
 </html>
